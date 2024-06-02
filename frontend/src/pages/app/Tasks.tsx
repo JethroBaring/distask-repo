04637140eb04a258c26a1b606ef1135f33dcee0b
Jethro Baring <baring.jeth@gmail.com>
2024-06-03 00:46:03 +0800

    added icon for update

--- frontend/src/pages/app/Tasks.tsx
+++ frontend/src/pages/app/Tasks.tsx
@@ -152,7 +152,7 @@ export const Tasks = () => {
       });
 
       const data = await response.json();
-      console.log("here")
+      console.log('here');
       console.log(data);
       if (response.ok) {
         const newTasks = tasks.map((column) => ({
@@ -285,25 +285,52 @@ export const Tasks = () => {
                             </div>
                             <div className='flex justify-between'>
                               <div className='flex gap-2 items-center'>
-                                <div className='w-8 rounded-full bg-slate-300 h-8 flex justify-center items-center'>{task.user.email.charAt(0).toUpperCase()}{task.user.email.charAt(1)}</div>
+                                <div className='w-8 rounded-full bg-slate-300 h-8 flex justify-center items-center'>
+                                  {task.user.email.charAt(0).toUpperCase()}
+                                  {task.user.email.charAt(1)}
+                                </div>
                                 <div className='text-sm'>{task.user.email}</div>
                               </div>
-                              <button onClick={() => deleteTask(task.id)}>
-                                <svg
-                                  xmlns='http://www.w3.org/2000/svg'
-                                  fill='none'
-                                  viewBox='0 0 24 24'
-                                  strokeWidth={1.5}
-                                  stroke='currentColor'
-                                  className='size-6'
+                              <div className='flex gap-2'>
+                                <button
+                                  onClick={() =>
+                                    document
+                                      .getElementById('update_task')
+                                      .showModal()
+                                  }
                                 >
-                                  <path
-                                    strokeLinecap='round'
-                                    strokeLinejoin='round'
-                                    d='m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0'
-                                  />
-                                </svg>
-                              </button>
+                                  <svg
+                                    xmlns='http://www.w3.org/2000/svg'
+                                    fill='none'
+                                    viewBox='0 0 24 24'
+                                    strokeWidth={1.5}
+                                    stroke='currentColor'
+                                    className='size-6'
+                                  >
+                                    <path
+                                      strokeLinecap='round'
+                                      strokeLinejoin='round'
+                                      d='m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10'
+                                    />
+                                  </svg>
+                                </button>
+                                <button onClick={() => deleteTask(task.id)}>
+                                  <svg
+                                    xmlns='http://www.w3.org/2000/svg'
+                                    fill='none'
+                                    viewBox='0 0 24 24'
+                                    strokeWidth={1.5}
+                                    stroke='currentColor'
+                                    className='size-6'
+                                  >
+                                    <path
+                                      strokeLinecap='round'
+                                      strokeLinejoin='round'
+                                      d='m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0'
+                                    />
+                                  </svg>
+                                </button>
+                              </div>
                             </div>
                           </Card>
                         </div>
@@ -341,7 +368,8 @@ export const Tasks = () => {
           <button>close</button>
         </form>
       </dialog>
-      <dialog id='task' className='modal'>
+      {/* Mao ni paras update */}
+      <dialog id='update_task' className='modal'>
         <div className='modal-box'>
           <h3 className='font-bold text-lg'>Hello!</h3>
           <p className='py-4'>Press ESC key or click outside to close</p>
